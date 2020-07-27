d362baca976d25ae3421742adf1608b9b7940d59
Navneet Anand <59524376+n4vn337@users.noreply.github.com>
2020-07-27 12:04:28 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# read -p 'IP Addr: ' ip
 
-# dirsearch.py -e php,txt,zip -u https://$ip -w db/dicc.txt -t 20
-# nmap -A -T4 -oA nmap $ip
+read -p 'IP Addr: ' ip
 
-# open tabs for each
-echo 'a script by n4vn337'
-echo 'visit at https://www.n4vn337.xyz/'
-
-# export TITLE_DEFAULT='hello'
-gnome-terminal --tab -- bash -ic "figlet n4vn337;echo [+] Starting nmap SCAN;pwd; pwd; exec bash;"
-gnome-terminal --tab -- bash -ic "figlet n4vn337;echo [+] Starting dirsearch SCAN; ls; exec bash;"
-gnome-terminal --tab -- bash -ic "figlet n4vn337;echo [+] Continue your job here; echo hello; exec bash;"
+#scipt on same page
+figlet n4vn337
+echo "[+] Creating Directories and redirecting to output"
+mkdir -p ~/Desktop/results/$ip && cd ~/Desktop/results/$ip && mkdir nmap gobuster nikto
+echo "Thank You For Downloading This Tool!"
+# open tabs for each operation
+gnome-terminal --tab -- bash -ic "cd nmap; figlet n4vn337; echo '[+] Starting nmap SCAN'; nmap -sC -sV -Pn -oA initial $ip; echo 'Thank You For Using This Tool!'; exec bash;"
+gnome-terminal --tab -- bash -ic "cd nikto; figlet n4vn337; echo '[+] Starting nikto SCAN'; nikto -h http://$ip -o nikto.txt; echo 'Thank You For Using This Tool!'; exec bash;"
+gnome-terminal --tab -- bash -ic "cd gobuster; figlet n4vn337;echo [+] Starting gobuster SCAN; gobuster dir -e -u http://$ip -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 40 -x php,txt,html -o gobuster.txt;echo 'Thank You For Using This Tool!'; exec bash;"
+gnome-terminal --tab -- bash -ic "cd ~; figlet n4vn337; echo [+] Continue your job here; Thank You for using this tool; exec bash;"
