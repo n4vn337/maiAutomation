e91ff7138270ea80fb53adb17e6b54fcbc3c7e41
Navneet Anand <59524376+n4vn337@users.noreply.github.com>
2020-07-30 08:45:31 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# read -p 'IP Addr: ' ip
 #scipt on same page
 figlet n4vn337
 echo "[+] Creating Directories and redirecting to output"
 mkdir -p ~/Desktop/results/$1 && cd ~/Desktop/results/$1 && mkdir nmap gobuster nikto
-# echo '[+] Opening the ip On Firefox' ;firefox $1 &
 echo '[+] Continue your job here'
 echo "Thank You For Using This Tool!"
-# open tabs for each operation
 
+# open tab-terminals for each operation
+#  nmap
 gnome-terminal --tab --title="nmap scan" -- bash -ic "cd nmap; figlet n4vn337; echo '[+] Starting nmap SCAN'; echo '[+] execution code: nmap -sC -sV -Pn -oA initial $1'; nmap -sC -sV -Pn -oA initial $1; echo 'Thank You For Using This Tool!'; exec bash;"
+# nikto
 gnome-terminal --tab --title="nikto scan" -- bash -ic "cd nikto; figlet n4vn337; echo '[+] Starting nikto SCAN'; echo '[+] execution code: nikto -h http://$1 -o nikto.txt'; nikto -h http://$1 -o nikto.txt; echo 'Thank You For Using This Tool!'; exec bash;"
+# goobuster with wordlist medium for better results
 gnome-terminal --tab --title="gobuster scan" -- bash -ic "cd gobuster; figlet n4vn337;echo [+] Starting gobuster SCAN;echo '[+] execution code: gobuster dir -e -u http://$1 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 40 -x php,txt,html -o gobuster.txt';gobuster dir -e -u http://$1 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 40 -x php,txt,html -o gobuster.txt;echo 'Thank You For Using This Tool!'; exec bash;"
 
- firefox $1 &
+# opening firefox
+firefox $1 &
