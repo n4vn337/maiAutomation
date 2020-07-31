6eecb9c5f8d490fdd593d9d56c6c1118d8424930
Navneet Anand <59524376+n4vn337@users.noreply.github.com>
2020-07-31 04:53:22 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #scipt on same page
-figlet n4vn337
+figlet maiAutomation; echo 'by n4vn337'
 echo "[+] Creating Directories and redirecting to output"
 mkdir -p ~/Desktop/results/$1 && cd ~/Desktop/results/$1 && mkdir nmap gobuster nikto
 echo '[+] Continue your job here'
@@ -9,11 +9,11 @@ echo "Thank You For Using This Tool!"
 
 # open tab-terminals for each operation
 #  nmap
-gnome-terminal --tab --title="nmap scan" -- bash -ic "cd nmap; figlet n4vn337; echo '[+] Starting nmap SCAN'; echo '[+] execution code: nmap -sC -sV -Pn -oA initial $1'; nmap -sC -sV -Pn -oA initial $1; echo 'Thank You For Using This Tool!'; exec bash;"
+gnome-terminal --tab --title="nmap scan" -- bash -ic "cd nmap; figlet maiAutomation; echo 'by n4vn337'; echo '[+] Starting nmap SCAN'; echo '[+] execution code: nmap -sC -sV -Pn -oA initial $1'; nmap -sC -sV -Pn -oA initial $1; echo 'Thank You For Using This Tool!'; exec bash;"
 # nikto
-gnome-terminal --tab --title="nikto scan" -- bash -ic "cd nikto; figlet n4vn337; echo '[+] Starting nikto SCAN'; echo '[+] execution code: nikto -h http://$1 -o nikto.txt'; nikto -h http://$1 -o nikto.txt; echo 'Thank You For Using This Tool!'; exec bash;"
+gnome-terminal --tab --title="nikto scan" -- bash -ic "cd nikto; figlet maiAutomation; echo 'by n4vn337'; echo '[+] Starting nikto SCAN'; echo '[+] execution code: nikto -h http://$1 -o nikto.txt'; nikto -h http://$1 -o nikto.txt; echo 'Thank You For Using This Tool!'; exec bash;"
 # goobuster with wordlist medium for better results
-gnome-terminal --tab --title="gobuster scan" -- bash -ic "cd gobuster; figlet n4vn337;echo [+] Starting gobuster SCAN;echo '[+] execution code: gobuster dir -e -u http://$1 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 40 -x php,txt,html -o gobuster.txt';gobuster dir -e -u http://$1 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 40 -x php,txt,html -o gobuster.txt;echo 'Thank You For Using This Tool!'; exec bash;"
+gnome-terminal --tab --title="gobuster scan" -- bash -ic "cd gobuster; figlet maiAutomation; echo 'by n4vn337';echo [+] Starting gobuster SCAN;echo '[+] execution code: gobuster dir -e -u http://$1 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 40 -x php,txt,html -o gobuster.txt';gobuster dir -e -u http://$1 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 40 -x php,txt,html -o gobuster.txt;echo 'Thank You For Using This Tool!'; exec bash;"
 
 # opening firefox
 firefox $1 &
